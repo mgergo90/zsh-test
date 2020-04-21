068a39dfa3f2bce2f794caa35a95429231e75bcc
Gary <gergo.nik.2009@gmail.com>
2020-04-21 11:55:45 +0200

    update lock file and remove duplicate

--- app/types/index.d.ts
+++ app/types/index.d.ts
@@ -1,19 +1,31 @@
-import { Reducer, Store, Observable } from 'redux';
+import { Reducer, Store, Observer } from 'redux';
 import { RouterState } from 'connected-react-router';
-import { ContainerState as LanguageProviderState } from 'containers/LanguageProvider/types';
+import { Observable, Subject } from 'rxjs';
 import { ActionType } from 'typesafe-actions';
+import { Epic } from 'redux-observable';
+
+import { ContainerState as LanguageProviderState } from 'containers/LanguageProvider/types';
+import { ContainerState as AppState } from 'containers/App/types';
+import { ContainerState as HomeState } from 'containers/HomePage/types';
 
 export type Action = ActionType<any>;
 
 export interface InjectedStore extends Store {
   injectedReducers: any;
-  injectedEpics: any;
-  epic$: any;
+  epic$: Observable;
+  injectedEpics: {
+    [key: string]: Epic;
+  };
 }
 
 export interface InjectReducerParams {
   key: keyof ApplicationRootState;
   reducer: Reducer<any, any>;
+}
+
+export interface InjectedEpicParams {
+  key: string;
+  epic: Epic;
 }
 
 export interface InjectSagaParams {
@@ -25,7 +37,11 @@ export interface InjectSagaParams {
 // Your root reducer type, which is your redux state types also
 export interface ApplicationRootState {
   readonly router: RouterState;
+  readonly global: AppState;
   readonly language: LanguageProviderState;
+  readonly home: HomeState;
   // for testing purposes
   readonly test: any;
 }
+
+export type WithState<T> = [T, ApplicationRootState];
